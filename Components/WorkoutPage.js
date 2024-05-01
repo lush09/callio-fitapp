1161d20912c91ca62d9ba551424ae5ffa197d003
Joshua Dave R. Buenaventura <joshuadavebee@gmail.com>
2024-05-01 15:40:27 +0800

    gfgfg

--- Components/WorkoutPage.js
+++ Components/WorkoutPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
-import Avatar from '../assets/Avatars/Male/CALLIO.png';
+import { StyleSheet, View, Text, Image, Dimensions } from 'react-native';
+import Avatar from '../assets/Avatars/Male/body.png';
+import Arrow from '../assets/arrows.png'
+
+var screenWidth = Dimensions.get('window').width;
+var width = screenWidth * .7;
+
 const WorkoutPage = () => {
   return (
     <View style={styles.workoutBody}>
-      <Image source={Avatar} style={styles.avatar}/>
+      <Text style={styles.heading}>Pick a body part to work on:</Text>
+      <View style={styles.workoutContainer}>
+        <Image source={Avatar} style={styles.avatar}/>
+        <Image source={Arrow} style={styles.arrow}/>
+      </View>
+      
     </View>
   );
 }
@@ -18,10 +28,24 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
   avatar: {
-    width: 500, 
-    height: 500, 
-    tintColor: '#FFFFFF', 
+    width: width,
+    height: width * 2.48,
   },
+  arrow: {
+    width: width,
+    height: width * 2.48,
+    left: '-20%',
+  },
+  workoutContainer:{
+    left: '-20%',
+    flexDirection: 'row',
+  },
+  heading: {
+    fontSize: 20,
+    padding: 10,
+    fontFamily: 'Poppins-Bold',
+    color: '#ffffff',
+  }
 });
 
 export default WorkoutPage;
