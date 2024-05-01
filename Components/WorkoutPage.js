05993e3e5e88ad4b12f28e658a5db521d4e34f7b
Joshua Dave R. Buenaventura <joshuadavebee@gmail.com>
2024-05-01 16:45:50 +0800

    jghjfghj

--- Components/WorkoutPage.js
+++ Components/WorkoutPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, Image, Dimensions,TouchableOpacity } from 'react-native';
 import Avatar from '../assets/Avatars/Male/body.png';
-import Arrow from '../assets/arrows.png'
+import Arrow from '../assets/arrows.png';
+import Arm from '../assets/Buttons/arm.png';
+import LowerBody from '../assets/Buttons/lowerbody.png';
+import UpperBody from '../assets/Buttons/upperbody.png';
 
 var screenWidth = Dimensions.get('window').width;
-var width = screenWidth * .7;
+var width = screenWidth * .6;
+var btnWidth = screenWidth * .4;
 
 const WorkoutPage = () => {
   return (
@@ -13,6 +17,17 @@ const WorkoutPage = () => {
       <View style={styles.workoutContainer}>
         <Image source={Avatar} style={styles.avatar}/>
         <Image source={Arrow} style={styles.arrow}/>
+        <View style={styles.textArea}>
+          <TouchableOpacity>
+            <Image source={UpperBody} style={styles.btn1}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={Arm} style={styles.btn2}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={LowerBody} style={styles.btn3}/>
+          </TouchableOpacity>
+        </View>
       </View>
       
     </View>
@@ -27,6 +42,10 @@ const styles = StyleSheet.create({
   workoutText: {
     color: '#FFFFFF',
   },
+  textArea: {
+    left: '-25%',
+    color: '#FFFFFF',
+  },
   avatar: {
     width: width,
     height: width * 2.48,
@@ -37,14 +56,34 @@ const styles = StyleSheet.create({
     left: '-20%',
   },
   workoutContainer:{
-    left: '-20%',
+    left: '-18%',
     flexDirection: 'row',
   },
   heading: {
-    fontSize: 20,
+    fontSize: 32,
     padding: 10,
     fontFamily: 'Poppins-Bold',
     color: '#ffffff',
+  },
+  text: {
+    fontSize: 30,
+    padding: 10,
+    fontFamily: 'Poppins-Bold',
+    color: '#ffffff',
+  },
+  btn1: {
+    width:btnWidth,
+    height: btnWidth * .4,
+    top:'50%',
+  },
+  btn2: {
+    top:'80%',
+    width:btnWidth,
+    height: btnWidth * .4,
+  },
+  btn3: {
+    width:btnWidth,
+    height: btnWidth * .4,
   }
 });
 
